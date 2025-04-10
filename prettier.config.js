b9c1799326d1d89da56ed59cf7412baf40c81d98
Emrin <emrin95@gmail.com>
2025-04-10 10:46:38 +0200

    feat(app): adds dev tools

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // printWidth: 120,
+  // bracketSameLine: false,
+  // bracketSpacing: true,
+  arrowParens: "always",
+  singleQuote: false,
+  tabWidth: 2,
+  semi: false,
+  trailingComma: "always",
+  plugins: ["prettier-plugin-tailwindcss"],
+}
+// npx prettier . --write
