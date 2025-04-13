5f28caaedeab783aa4db61f13639a53c094d4092
Emrin <emrin95@gmail.com>
2025-04-13 13:41:51 +0200

    feat(app): db seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,11 +2,21 @@
  * Seeds DB.
  * */
 import { PrismaClient } from "@prisma/client"
+import animals from "./data/animals"
 
 const prisma = new PrismaClient()
 
 async function main() {
-  // todo populate db with example values
+  const currentAnimals = await prisma.animal.count()
+  console.log(`Found ${currentAnimals} recorded animals.`)
+  if (!currentAnimals) {
+    console.log("Adding animals...")
+    const newAnimals = await prisma.animal.createMany({
+      data: animals,
+      skipDuplicates: true,
+    })
+    console.log(`Added ${animals.length} animals to the record.`)
+  }
 }
 
 main()
--- src/app/(main)/game/page.tsx
+++ src/app/(main)/game/page.tsx
@@ -4,6 +4,7 @@ import { getIronSession } from "iron-session"
 import { SessionData, sessionOptions } from "src/auth"
 import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
+import prisma from "lib/prisma"
 import React from "react"
 
 export async function generateMetadata() {
@@ -23,13 +24,27 @@ export default async function Game() {
   const locale = await getCookieLang()
   const t = await createTranslator(locale, "Game")
 
+  const animalsCount = await prisma.animal.count()
+
+
+
   return (
     <div>
       <h3 className="text-2xl text-center mt-10 font-semibold">Welcome to the guessing game!</h3>
-      <h3 className="text-center text-slate-500">This page uses client-side JavaScript for demo purposes!</h3>
+      <h3 className="text-center text-slate-500 mb-10">This page uses client-side JavaScript for demo purposes!</h3>
+
+      <h3 className="text-center text-gray-300 text-xl font-semibold">Rules:</h3>
+      <ul className="text-center text-gray-300">
+        <li>You will be shown images of extinct animals, as well as their names.</li>
+        <li>You must guess as many details as you can about the animals, without looking it up!</li>
+        <li>Your score increases the closer you are to the truth!</li>
+        <li>After 10 guesses, your final score is displayed in a leaderboard!</li>
+      </ul>
+
+      <p className="text-center py-10">There are currently {animalsCount} recorded animals!</p>
+
 
       <h3 className="text-center py-10">#TODO</h3>
-
     </div>
   );
 }
